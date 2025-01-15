3a7e9b478fb82ae8a1dacd482d9ca58fc77f28ef
ArianaRaschidFarrokhi <arianarfarrokhi@gmail.com>
2025-01-15 15:11:12 -0500

    Add assignment number 1

---
+++ assignments/assignment_script_1.R
@@ -0,0 +1,14 @@
+brickman_variables("all")
+buoys=gom_buoys()
+coast=read_coastline()
+db=brickman_database()
+buoys=buoys|>filter(id=="M01")
+db = db |> filter(scenario == "PRESENT", interval == "mon")
+covars=read_brickman(db)
+x = extract_brickman(covars, buoys, form = "wide")
+mutate(month = factor(month, levels = month.abb))
+ggplot(data = x,
+       mapping = aes(x = month, y = SST)) +
+  geom_point() + 
+  labs(title = "RCP4.5 2055 SST at buoy M01", y= "SST (C)")
+
