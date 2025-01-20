8d0b331159c29be06d8e92f0db70a97b3deeef9c
ArianaRaschidFarrokhi <arianarfarrokhi@gmail.com>
2025-01-20 14:44:15 -0500

    Defined function read_model_input. Set default to species Fratercula arctica and month of March.

--- functions/model_input.R
+++ functions/model_input.R
@@ -4,9 +4,25 @@
 #' @param mon chr month abbreviation
 #' @param approach chr, one of "greedy" or "conservative"
 #' @param path chr the path to the data directory
-read_model_input = function(scientificname = "Mola mola",
-                            mon = "Jan",
+read_model_input = function(scientificname = "Fratercula arctica",
+                            mon = "Mar",
                             approach = "greedy",
                             path = data_path("model_input")){
   # your part goes in here
+  species=scientificname
+  
+  if( approach == "greedy"){
+  filename = sprintf("%s-%s-greedy_input.gpkg", 
+                     gsub(" ", "_", species),
+                     mon)}
+  
+  if( approach == "conservative"){
+  filename = sprintf("%s-%s-conservative_input.gpkg", 
+                     gsub(" ", "_", species),
+                     mon)}
+  x=file.path(path,filename)
+  model_input= read_sf(x) 
+    
+  return(model_input)
+  
 }
